bcfba69c278be7f7fb3287b6be34f90eeaedb0e1
patrickjaja <patrickjajaa@gmail.com>
2026-01-30 11:39:51 +0100

    Switch basic auth for Diagnostik from .htaccess to PHP controller
    .htaccess <If> directive not supported on shared hosting. Moved basic
    auth check into ServicesController for all /leistungen/diagnostik routes.

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -1,8 +1,25 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class ServicesController extends BaseController
 {
+    private function requireDiagnostikAuth(Request $request): ?Response
+    {
+        $user = $request->headers->get('PHP_AUTH_USER');
+        $pass = $request->headers->get('PHP_AUTH_PW');
+
+        if ($user !== 'patient' || $pass !== 'neuss2026') {
+            $response = new Response('Zugang verweigert.', 401);
+            $response->headers->set('WWW-Authenticate', 'Basic realm="Geschützter Bereich - Zugangsdaten erhalten Sie von Ihrer Therapeutin"');
+            return $response;
+        }
+
+        return null;
+    }
+
     public function index()
     {
         return $this->render('pages/services.html.twig', [
@@ -31,36 +48,41 @@ class ServicesController extends BaseController
         ]);
     }
 
-    public function diagnostik()
+    public function diagnostik(Request $request)
     {
+        if ($deny = $this->requireDiagnostikAuth($request)) return $deny;
         return $this->render('pages/services/diagnostik.html.twig', [
             'current_page' => 'diagnostik'
         ]);
     }
 
-    public function diagnostikBdi()
+    public function diagnostikBdi(Request $request)
     {
+        if ($deny = $this->requireDiagnostikAuth($request)) return $deny;
         return $this->render('pages/services/diagnostik/bdi-ii.html.twig', [
             'current_page' => 'diagnostik'
         ]);
     }
 
-    public function diagnostikScid()
+    public function diagnostikScid(Request $request)
     {
+        if ($deny = $this->requireDiagnostikAuth($request)) return $deny;
         return $this->render('pages/services/diagnostik/scid-5-spq.html.twig', [
             'current_page' => 'diagnostik'
         ]);
     }
 
-    public function diagnostikScl()
+    public function diagnostikScl(Request $request)
     {
+        if ($deny = $this->requireDiagnostikAuth($request)) return $deny;
         return $this->render('pages/services/diagnostik/scl-90.html.twig', [
             'current_page' => 'diagnostik'
         ]);
     }
 
-    public function diagnostikCtq()
+    public function diagnostikCtq(Request $request)
     {
+        if ($deny = $this->requireDiagnostikAuth($request)) return $deny;
         return $this->render('pages/services/diagnostik/ctq.html.twig', [
             'current_page' => 'diagnostik'
         ]);
